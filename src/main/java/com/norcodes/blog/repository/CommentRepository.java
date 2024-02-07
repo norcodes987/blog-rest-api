af458569dd2c9f61acc2ea5b79c12972b28d9a02
norcodes987 <norcodes987@gmail.com>
2024-02-07 15:10:58 +0800

    crud rest apis for comments

---
+++ src/main/java/com/norcodes/blog/repository/CommentRepository.java
@@ -0,0 +1,10 @@
+package com.norcodes.blog.repository;
+
+import com.norcodes.blog.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByPostId(long postId);
+}
---
+++ src/main/java/com/norcodes/blog/service/CommentService.java
@@ -0,0 +1,17 @@
+package com.norcodes.blog.service;
+
+import com.norcodes.blog.payload.CommentDto;
+
+import java.util.List;
+
+public interface CommentService {
+    CommentDto createComment(long postId, CommentDto commentDto);
+
+    List<CommentDto> getCommentsByPostId(long postId);
+
+    CommentDto getCommentById(Long postId, Long commentId);
+
+    CommentDto updateComment(Long postId, Long commentId, CommentDto commentRequest);
+
+    void deleteComment(Long postId, Long commentId);
+}
