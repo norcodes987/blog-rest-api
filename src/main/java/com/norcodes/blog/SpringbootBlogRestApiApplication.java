6681904c86160c9b5f3a11ebe2bb9470050be5b9
norcodes987 <norcodes987@gmail.com>
2024-03-08 22:13:03 +0800

    add prod deployment

--- src/main/java/com/norcodes/blog/SpringbootBlogRestApiApplication.java
+++ src/main/java/com/norcodes/blog/SpringbootBlogRestApiApplication.java
@@ -1,11 +1,15 @@
 package com.norcodes.blog;
 
+import com.norcodes.blog.entity.Role;
+import com.norcodes.blog.repository.RoleRepository;
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -18,7 +22,7 @@ import org.springframework.context.annotation.Bean;
 				version = "v1.0"
 		)
 )
-public class SpringbootBlogRestApiApplication {
+public class SpringbootBlogRestApiApplication implements CommandLineRunner {
 	@Bean
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
@@ -28,4 +32,16 @@ public class SpringbootBlogRestApiApplication {
 		SpringApplication.run(SpringbootBlogRestApiApplication.class, args);
 	}
 
+	@Autowired
+	private RoleRepository roleRepository;
+	@Override
+	public void run(String... args) throws Exception {
+		Role adminRole = new Role();
+		adminRole.setName("ROLE_ADMIN");
+		roleRepository.save(adminRole);
+
+		Role userRole = new Role();
+		userRole.setName("ROLE_USER");
+		roleRepository.save(userRole);
+	}
 }
